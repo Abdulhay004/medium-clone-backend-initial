cd25dc2b7617a161d645486fcac89726c10b1a56
Your Name <your.email@example.com>
2024-09-04 13:12:24 +0500

    recreate

--- core/settings.py
+++ core/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-star
+# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 from decouple import config
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "articles",
 ]
 
 MIDDLEWARE = [
