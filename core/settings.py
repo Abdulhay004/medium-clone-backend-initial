9c5d848ff4ec9f9b00b26312742001b9ffa219ff
Your Name <your.email@example.com>
2024-09-07 20:02:38 +0500

    recreate

--- core/settings.py
+++ core/settings.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the proect like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = "django-insecure-bos)q+o=7(ii#0x_-gjnz16(4$jg81lo1sm+ltk@z%0u20*t%g"
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
 from decouple import config
 
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-bos)q+o=7(ii#0x_-gjnz16(4$jg81lo1sm+ltk@z%0u20*t%g')
+SECRET_KEY = config('SECRET_KEY', default='hjg^&%**%%^*GHVGJHGKJGKH')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
@@ -22,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "articles",
 ]
 
 MIDDLEWARE = [
