110ddbc7093d50cad46cff20da4de8e05fb4dfac
Your Name <your.email@example.com>
2024-09-18 17:12:08 +0500

    any

--- core/settings.py
+++ core/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 # Build paths inside the ect like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for
+# Quick-start development ettings - unsuitable for
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 from decouple import config
