33e16eb1a3feb7c7f7c52c9a8f630837cf882653
Your Name <your.email@example.com>
2024-09-19 17:38:12 +0500

    any

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY', default='hjg^&%**%%^*GHVGJHGKJGKH')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
