e5b9575699b656157af06ede0994ebf09943a5d9
Your Name <your.email@example.com>
2024-09-20 22:32:09 +0500

    any

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
