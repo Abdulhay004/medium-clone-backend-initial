eae3d5100f3e0527c1304e365552b8a29a73d1a9
Abdulhay004 <abdulhaytolibjonov8@gmail.com>
2024-09-28 21:24:01 +0500

    any

--- articles/models.py
+++ articles/models.py
@@ -33,6 +33,7 @@ class Article(models.Model):
     midle_name = models.CharField(max_length=200, blank=True)
     email = models.EmailField()
     avatar = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
